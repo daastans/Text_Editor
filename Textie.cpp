65616617ef997e2ee337599f458070b9a905e3bf
Aman Sharma <amansharma.pcmip@gmail.com>
2019-04-13 14:03:35 +0530

    Turning Flags off

--- Textie.cpp
+++ Textie.cpp
@@ -11,16 +11,25 @@ void DisableRawMode() {
 void EnableRawMode()
 {
 
-  struct termios raw=original_termios;
+
   tcgetattr(STDIN_FILENO,&original_termios);
   atexit(DisableRawMode);//from stdlib.h
-  raw.c_lflag&=~(ECHO);
+  struct termios raw=original_termios;
+  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_lflag&=~(ECHO | ICANON | ISIG | IEXTEN);
   tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
+
 }
 int main()
 {
   EnableRawMode();
   char c;
-  std::cout<<"Textie Version : 1 \n";
-  while(read(STDIN_FILENO,&c,1)==1 && c!='q');
+  std::cout<<"Textie Version : 1 \r\n";// \r is use to bring pointer to start of line
+  while(read(STDIN_FILENO,&c,1)==1 && c!='q')
+  {
+    if(iscntrl(c)) std::cout<<int(c)<<'\r'<<'\n';
+    else std::cout<<int(c)<<"("<<c<<")"<<'\r'<<'\n';
+
+  }
 }
