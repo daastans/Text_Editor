7e4a785e65e49e16a9f9ee659e487f9e0d9bc4ab
Aman Sharma <amansharma.pcmip@gmail.com>
2019-04-13 12:51:56 +0530

    Turning ECho off

--- Textie.cpp
+++ Textie.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 #include<unistd.h>
+#include<termios.h>
 
+void EnableRawMode()
+{
+  struct termios raw;
+  tcgetattr(STDIN_FILENO,&raw);
+  raw.c_lflag&=~(ECHO);
+  tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
+}
 int main()
 {
+  EnableRawMode();
   char c;
   std::cout<<"Textie Version : 1 \n";
   while(read(STDIN_FILENO,&c,1)==1 && c!='q');
