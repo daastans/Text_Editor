796b98c7cf9e19e97d65fd22d92ab24b609c1db0
Aman Sharma <amansharma.pcmip@gmail.com>
2019-04-14 20:40:30 +0530

    Error Handling

--- Textie.cpp
+++ Textie.cpp
@@ -1,18 +1,24 @@
 #include<iostream>
 #include<unistd.h>
 #include<stdlib.h>
-
+#include<stdio.h>
+#include<errno.h>
 #include<termios.h>
 
 struct termios original_termios;
+void die(const char* s)
+{
+  perror(s);
+  exit(1);
+}
 void DisableRawMode() {
-  tcsetattr(STDIN_FILENO,TCSAFLUSH,&original_termios);
+  if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&original_termios)==-1)
+    die("tcsetattr");
 }
 void EnableRawMode()
 {
-
-
-  tcgetattr(STDIN_FILENO,&original_termios);
+  if(tcgetattr(STDIN_FILENO,&original_termios)==-1)
+    die("tcgetattr");
   atexit(DisableRawMode);//from stdlib.h
   struct termios raw=original_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
@@ -20,7 +26,8 @@ void EnableRawMode()
   raw.c_lflag&=~(ECHO | ICANON | ISIG | IEXTEN);
   raw.c_cc[VMIN] = 0;//minimum character to read before returning input
   raw.c_cc[VTIME] = 1;//sets time to wait before returning input
-  tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
+
+  if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw)==-1) die("tcsetattr");
 
 }
 int main()
@@ -28,10 +35,12 @@ int main()
   EnableRawMode();
   char c;
   std::cout<<"Textie Version : 1 \r\n";// \r is use to bring pointer to start of line
-  while(read(STDIN_FILENO,&c,1)==1 && c!='q')
+  while(1)
   {
+    read(STDIN_FILENO,&c,1);
     if(iscntrl(c)) std::cout<<int(c)<<'\r'<<'\n';
     else std::cout<<int(c)<<"("<<c<<")"<<'\r'<<'\n';
+    if(c=='q') break;
 
   }
 }
